db48ed80df016db5ca608dd12c6724faacda3e7c
asakaicode <tubakaito5100@gmail.com>
2024-09-01 03:01:05 +0900

    fix: deploy with github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/asakaicode.github.io",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
